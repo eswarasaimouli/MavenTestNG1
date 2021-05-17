aac11805782a62a0bee978c59fea88647835a896
sridevi kalaga <saimouli.93@gmail.com>
2021-05-17 23:27:32 +0530

    new code is comitted with Version5.01 changes

---
+++ src/test/java/MavenOne/TestingMaven/Day3Test - Copy - Copy.java
@@ -0,0 +1,24 @@
+package MavenOne.TestingMaven;
+
+import org.testng.annotations.Test;
+
+public class Day3Test {
+
+	@Test
+	public void CarLoanMobileDay3() {
+		System.out.println("CarLoanMobileDay3");
+	}
+	@Test
+	public void CarLoanWebDay3() {
+		System.out.println("CarLoanWebDay3");
+	}
+	@Test
+	public void BusLoanMobileDay3() {
+		System.out.println("BusLoanMobileDay3");
+	}
+	@Test
+	public void BusLoanWebDay3() {
+		System.out.println("BusLoanWebDay3");
+	}
+	
+}
